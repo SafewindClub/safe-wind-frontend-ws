8eda01cdea1794c10560b46043b6910f4ba8356d
Leaftrip <Wy3476778966@163.com>
2025-05-30 19:21:30 +0800

    第一批组件上传
    上传第一批共计3个组件，以及配套资源，Header(导航栏)，Footer(页尾),CarouselPic(图片轮换框)

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+<!--  默认主页，后续如果需要再进行修改  -->
+  <tiny-container :pattern="pattern">
+    <template #header>
+      <Header/>
+    </template>
+    <template #default >
+      <CarouselPic  />
+    </template>
+    <template #footer>
+      <Footer/>
+    </template>
+  </tiny-container>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Header from './components/header/Header.vue'
+import Footer from './components/footer/Footer.vue'
+import CarouselPic from './components/carouselpic/CarouselPic.vue'
+import {TinyContainer} from "@opentiny/vue";
+
+const pattern = ref('classic')
+</script>
+
+<style scoped>
+
+</style>
